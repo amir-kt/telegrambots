346a71b54b7288e60175eafd1b16f90b9bb00845
Amir <skho0021@student.monash.edu>
2023-03-19 21:41:18 +1100

    removed caching the game info logic

--- telegrambots/kcfixturebot/handlers/show_fixture.py
+++ telegrambots/kcfixturebot/handlers/show_fixture.py
@@ -15,11 +15,9 @@ async def fixture_text(message: types.Message, state: FSMContext):
             await message.answer(strings.no_team_selected)
             return
 
-        # Only scrape the game data if necessary in order to save time
-        if await state_manager.is_game_info_expired(state):
-            game_info = await scrape_game_info(team_name)
-            if game_info:
-                await state_manager.set_game_info(game_info, state)
+        game_info = await scrape_game_info(team_name)
+        if game_info:
+            await state_manager.set_game_info(game_info, state)
 
         await message.answer(
             strings.next_message(
