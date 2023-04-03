b866fd312ad77402e11c37225fcc07465b3bb8f7
Amir Toosi <amir.ghalb@gmail.com>
2023-04-04 00:39:54 +1000

    fixed import error

--- telegrambots/babyurlbot/handlers/help.py
+++ telegrambots/babyurlbot/handlers/help.py
@@ -1,11 +1,11 @@
-import strings
 from aiogram import types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
 from ..bot import dp
+from .strings import start_message
 
 
 @dp.message(Command(commands=["start"]))
 async def start(message: types.Message, state: FSMContext):
-    await message.reply(strings.start_message)
+    await message.reply(start_message)
